6d189488e79d021928be62a120ec78fbd5b2f9ca
Nicolas Brassard <nico@nitriques.com>
2011-07-19 14:50:35 -0400

    Added FR translation: Preparing for adding a "limit" param

---
+++ lang/lang.fr.php
@@ -0,0 +1,25 @@
+<?php
+
+	$about = array(
+		'name' => 'Italiano',
+		'author' => array(
+			'name' => 'Simone Economo',
+			'email' => 'my.ekoes@gmail.com',
+			'website' => 'http://lineheight.net',
+		),
+	);
+	
+	
+	/*
+	 * EXTENSION: Static Sections
+	 * Localisation strings
+	 */
+
+	$dictionary = array(
+
+		'Make this section static (i.e. a single entry section)' =>
+		'Rendre cette section statiques (i.e. une seule entrée)'
+
+	);
+
+?>
